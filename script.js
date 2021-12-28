e0f898513a3973d163806f5358bd0a7bc37f0a5e
sameer5858 <www.md.sameerkhan5858@gmail.com>
2021-12-28 16:57:06 +0530

    single rps game fuction made

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function computerPlay() {
   const result = choices[randomChoice];
   return result;
 }
+function result(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You Lose! Rock beats Scissors";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection === computerSelection) {
+    return "Tis a tie";
+  }
+}
