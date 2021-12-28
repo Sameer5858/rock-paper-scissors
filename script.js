537f23892c2e99c6ceebd819ae7b49030ad34458
sameer5858 <www.md.sameerkhan5858@gmail.com>
2021-12-28 21:58:27 +0530

    all done

--- script.js
+++ script.js
@@ -23,16 +23,27 @@ function playRound(playerSelection, computerSelection) {
     return "Tis a tie";
   }
 }
-let askQuestion;
-askQuestion = prompt("choose between rock paper and scissors");
-const playerSelection = askQuestion.toLowerCase();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
+  let askQuestion1 = prompt("choose between rock paper and scissors");
+  const playerSelection1 = askQuestion1.toLowerCase();
+  const computerSelection1 = computerPlay();
+  console.log(playRound(playerSelection1, computerSelection1));
+  let askQuestion2 = prompt("choose between rock paper and scissors");
+  const playerSelection2 = askQuestion2.toLowerCase();
+  const computerSelection2 = computerPlay();
+  console.log(playRound(playerSelection2, computerSelection2));
+  let askQuestion3 = prompt("choose between rock paper and scissors");
+  const playerSelection3 = askQuestion3.toLowerCase();
+  const computerSelection3 = computerPlay();
+  console.log(playRound(playerSelection3, computerSelection3));
+  let askQuestion4 = prompt("choose between rock paper and scissors");
+  const playerSelection4 = askQuestion4.toLowerCase();
+  const computerSelection4 = computerPlay();
+  console.log(playRound(playerSelection4, computerSelection4));
+  let askQuestion5 = prompt("choose between rock paper and scissors");
+  const playerSelection5 = askQuestion5.toLowerCase();
+  const computerSelection5 = computerPlay();
+  console.log(playRound(playerSelection5, computerSelection5));
 }
+console.log(game());
